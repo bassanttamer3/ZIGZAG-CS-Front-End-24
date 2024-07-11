ce898bcb28fcd31aec271a4606c97b9de4ab7a48
bassant <115741505+bassanttamer3@users.noreply.github.com>
2024-07-11 22:12:50 +0200

    Task

---
+++ Type Script/Type Annotations/Task5/TS.ts
@@ -0,0 +1,14 @@
+//function reportErrors(username, age: number) {
+    //let rank = "Professor";
+    //return `Username: ${username}`;
+    //console.log("We Will Not Reach Here");
+  //}
+  
+  //console.log(reportErrors("Elzero", 40));
+
+  function reportErrors(username: string, age: number) {
+    return `Username: ${username}, Age: ${age}`;
+  }
+  
+  console.log(reportErrors("Elzero", 40));
+  
